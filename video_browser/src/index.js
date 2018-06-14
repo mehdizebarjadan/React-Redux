e304c75b5cbdd57ef55ea8d89dfc380e18428736
MehdiZebarjadan <MehdiZebarjadan@yahoo.com>
2018-06-14 07:13:18 -0400

    Youtube search response

--- video_browser/src/index.js
+++ video_browser/src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyC4i1sjCvcpcX0KaH4bjj9dlExjA1MDGac';
+
+YTSearch({key: API_KEY, term: 'mehdizebarjadan'}, function(data) {
+  console.log(data);
+});
 
 // Create a new component which should produce some HTML
 const App = () => {
  return (
     <div>
       <SearchBar />
-    </div>  
+    </div>
   );
 }
 
