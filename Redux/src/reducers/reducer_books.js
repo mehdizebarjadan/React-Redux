7b2666440f7b0293c2d62db73ffeb1be48d212ce
MehdiZebarjadan <MehdiZebarjadan@yahoo.com>
2018-06-15 17:57:45 -0400

    Reducers

---
+++ Redux/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Where the Wild Things Are'},
+    { title: 'The Hitchhiker`s Guide to the Galaxy'},
+    { title: 'I Was Told There`d Be Cake'},
+    { title: 'four'}
+  ]
+}
