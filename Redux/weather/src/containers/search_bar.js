bb5d226ee118bfc3615028fccea186c3ab8087f3
MehdiZebarjadan <MehdiZebarjadan@yahoo.com>
2018-06-16 00:42:30 -0400

    Form Elements in React

--- Redux/weather/src/containers/search_bar.js
+++ Redux/weather/src/containers/search_bar.js
@@ -14,9 +14,16 @@ export default class SearchBar extends Component {
       this.setState ({ term: event.target.value });
   }
 
+  onFormSubmit(event) {
+    // Prevent to submit data in form tag
+    event.preventDefault();
+
+    // We need to go and fetch data
+  }
+
   render() {
     return (
-      <form className="input-group">
+      <form onSubmit={this.onFormSubmit} className="input-group">
         <input
           placeholder="Get a five-day forcast in your favorite cities"
           className="form-control"
