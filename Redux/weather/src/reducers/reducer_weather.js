1b72bffbf90b24dd1dea7d49cccc57a0b0d6feec
MehdiZebarjadan <MehdiZebarjadan@yahoo.com>
2018-06-16 01:29:00 -0400

    Avoiding State Mutations in Reducers

--- Redux/weather/src/reducers/reducer_weather.js
+++ Redux/weather/src/reducers/reducer_weather.js
@@ -1,7 +1,12 @@
 import { FETCH_WEATHER } from '../actions/index';
 
 export default function(state = [], action) {
-  console.log('Action received', action);
+  // console.log('Action received', action);
+  switch (action.type) {
+  case FETCH_WEATHER:
+    // return state.concat([action.payload.data]); //this line and the one below are identical
+    return [ action.payload.data, ...state ];   // [city, city, city] NOT [ city, [ city, city] ]
+  }
 
   return state;
 }
