d9ab02c1d30e1e1ee9876debe7c0d7429e0ce74c
MehdiZebarjadan <MehdiZebarjadan@yahoo.com>
2018-06-15 22:48:00 -0400

    Conditional Rendering

--- Redux/src/reducers/reducer_books.js
+++ Redux/src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    { title: 'Where the Wild Things Are'},
-    { title: 'The Hitchhiker`s Guide to the Galaxy'},
-    { title: 'I Was Told There`d Be Cake'},
-    { title: 'four'}
+    { title: 'Where the Wild Things Are', pages: 101},
+    { title: 'The Hitchhiker`s Guide to the Galaxy', pages: 39},
+    { title: 'I Was Told There`d Be Cake', pages: 85},
+    { title: 'four', pages: 1}
   ]
 }
