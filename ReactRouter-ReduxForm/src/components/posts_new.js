251d170273f5fcec0d2823be3e9c089eeecbef9e
MehdiZebarjadan <MehdiZebarjadan@yahoo.com>
2018-06-17 07:51:34 -0400

    Handling Form Submittal

--- ReactRouter-ReduxForm/src/components/posts_new.js
+++ ReactRouter-ReduxForm/src/components/posts_new.js
@@ -17,9 +17,15 @@ class PostsNew extends Component {
       );
     }
 
+  onSubmit(values) {
+    console.log(values);
+  }
+
   render() {
+    const { handleSubmit } = this.props;
+
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field
           label="Title For Post"
           name="title"
@@ -35,6 +41,7 @@ class PostsNew extends Component {
           name="content"
           component={this.renderField}
         />
+        <button type="submit" className="btn btn-primary">Submit</button>
       </form>
     );
   }
