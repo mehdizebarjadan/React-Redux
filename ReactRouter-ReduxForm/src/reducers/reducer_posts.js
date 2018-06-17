dd2aa5939bbad7c0b249899c1c18eb35e1f9bb72
MehdiZebarjadan <MehdiZebarjadan@yahoo.com>
2018-06-17 09:07:08 -0400

    Receiving New Posts

--- ReactRouter-ReduxForm/src/reducers/reducer_posts.js
+++ ReactRouter-ReduxForm/src/reducers/reducer_posts.js
@@ -3,6 +3,13 @@ import { FETCH_POSTS } from "../actions";
 
 export default function(state = {}, action) {
   switch (action.type) {
+    case FETCH_POST:
+          // const post = action.payload.data;
+          // const newState = { ...state };
+          // newState[post.id] = post;
+          // return newState;
+
+          return { ...state, [action.payload.data.id]: action.payload.data };
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, "id");
     default:
