0f6104887569e212b7ab080956dd85f360bb95f9
MehdiZebarjadan <MehdiZebarjadan@yahoo.com>
2018-06-17 10:50:52 -0400

    Data Dependencies

--- ReactRouter-ReduxForm/src/components/posts_index.js
+++ ReactRouter-ReduxForm/src/components/posts_index.js
@@ -6,14 +6,16 @@ import { fetchPosts } from "../actions";
 
 class PostsIndex extends Component {
   componentDidMount() {
-      this.props.fetchPosts();
-    }
+    this.props.fetchPosts();
+  }
 
   renderPosts() {
     return _.map(this.props.posts, post => {
       return (
         <li className="list-group-item" key={post.id}>
-          {post.title}
+          <Link to={`/posts/${post.id}`}>
+            {post.title}
+          </Link>
         </li>
       );
     });
--- ReactRouter-ReduxForm/src/components/posts_show.js
+++ ReactRouter-ReduxForm/src/components/posts_show.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { fetchPost } from "../actions";
 
@@ -9,9 +10,17 @@ class PostsShow extends Component {
   }
 
   render() {
+    const { post } = this.props;
+
+    if (!post) {
+      return <div>Loading...</div>;
+    }
+
     return (
       <div>
-        Posts Show!
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
       </div>
     );
   }
